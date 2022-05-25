ba6eb16bf96746ed2581f5c67521c312e1bb3f14
tetsu333 <70054989+tetsu333@users.noreply.github.com>
2022-05-25 23:00:09 +0900

    デフォルト値

--- src/index.js
+++ src/index.js
@@ -71,8 +71,11 @@
 // const message2 = `名前は${name}です。年齢は${age}です。`;
 // console.log(message2);
 
-const myProfile = ["tom", 20];
-// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
-const [name, age] = myProfile;
-const message4 = `名前は${name}です。年齢は${age}です。`;
-console.log(message4);
+// const myProfile = ["tom", 20];
+// // const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}です。`;
+// console.log(message4);
+
+const sayHello = (name = "太郎") => console.log(`こんにちは！${name}さん！`);
+sayHello("Tom");
