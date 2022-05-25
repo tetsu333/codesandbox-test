a21ce0511372cc4d87727b51077aad20fdba139d
tetsu333 <70054989+tetsu333@users.noreply.github.com>
2022-05-25 23:11:31 +0900

    スプレッド構文

--- src/index.js
+++ src/index.js
@@ -77,5 +77,33 @@
 // const message4 = `名前は${name}です。年齢は${age}です。`;
 // console.log(message4);
 
-const sayHello = (name = "太郎") => console.log(`こんにちは！${name}さん！`);
-sayHello("Tom");
+// const sayHello = (name = "太郎") => console.log(`こんにちは！${name}さん！`);
+// sayHello("Tom");
+
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+arr6[0] = 100;
+console.log(arr6);
+console.log(arr4);
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// const arr8 = arr4;
+// arr8[0] = 100;
+// console.log(arr4);
