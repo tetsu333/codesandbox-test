d523e9dc304cb544ac30c58bef4b12282bef06bb
tetsu333 <70054989+tetsu333@users.noreply.github.com>
2022-05-25 23:30:25 +0900

    三項演算子

--- src/index.js
+++ src/index.js
@@ -126,11 +126,26 @@ const nameArr = ["田中", "山田", "佐藤"];
 // })
 // console.log(newNumArr);
 
-const newNameArr = nameArr.map((name) => {
-  if (name === "佐藤") {
-    return name;
-  } else {
-    return `${name}さん`;
-  }
-});
-console.log(newNameArr);
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "佐藤") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
+
+// const val1 = 1 < 0 ? "trueです" : "falseです";
+// console.log(val1);
+
+// const num = 1300;
+// console.log(num.toLocaleString());
+
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください。";
+// console.log(formattedNum);
+
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "１００を超えています" : "許容範囲内です";
+};
+console.log(checkSum(50, 40));
